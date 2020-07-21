541692b89f712ceb708f823d7f030a20c9e459f8
adsonmuniz <adsonmuniz@gmail.com>
2020-07-21 12:42:27 -0300

    master: Melhoria da função de deletar diretório, agora deletando sub-diretorios associados

--- model/folder.php
+++ model/folder.php
@@ -32,27 +32,6 @@ class Folder {
         $this->name = $nome;
     }
 
-
-    // logica para salvar a pasta no banco
-    public function save() {
-        $query = "INSERT INTO folder (id_parent, name) VALUES ('$this->id_parent','$this->name')";
-        $resultQuery = mysqli_query($this->connection(),$query);
-        return $resultQuery;
-    }
-    
-    // logica para atualizar a pasta no banco
-    public function update() {
-        $query = "UPDATE folder SET name = '$this->name' WHERE id='$this->id'";
-        $resultQuery = mysqli_query($this->connection(),$query);
-        return $resultQuery;
-    }
-    
-    // logica para remover a pasta do banco
-    public function remove() {
-        $query = "DELETE FROM folder WHERE id='$this->id'";
-        $resultQuery = mysqli_query($this->connection(),$query);
-        return $resultQuery;
-    }
     
     // logica para listar todos pastas raízes do banco
     public function listAll() {
@@ -68,6 +47,42 @@ class Folder {
         return $resultQuery;
     }
 
+    // logica para salvar a pasta no banco
+    public function save() {
+        $query = "INSERT INTO folder (id_parent, name) VALUES ('$this->id_parent','$this->name')";
+        $resultQuery = mysqli_query($this->connection(),$query);
+        return $resultQuery;
+    }
+    
+    // logica para atualizar a pasta no banco
+    public function update() {
+        $query = "UPDATE folder SET name = '$this->name' WHERE id='$this->id'";
+        $resultQuery = mysqli_query($this->connection(),$query);
+        return $resultQuery;
+    }
+    
+    // logica para remover a pasta e suas sub-pastas do banco
+    public function remove() {
+        $arrayIds = array();
+        array_push($arrayIds, $this->id);
+
+        $arrayLenght = count($arrayIds);
+        for($it=0; $it<$arrayLenght; $it++) {
+            $query = "SELECT id FROM folder WHERE id_parent='$arrayIds[$it]'";
+            $resultQuery = mysqli_query($this->connection(),$query);
+            
+            while($r = mysqli_fetch_array($resultQuery)) {
+                array_push($arrayIds, $r['id']);
+            }
+            $arrayLenght = count($arrayIds);
+        }
+        foreach($arrayIds as $item) {
+            $query = "DELETE FROM folder WHERE id='$item'";
+            $resultQuery = mysqli_query($this->connection(),$query);
+        }
+        return $resultQuery;
+    }
+
     // Connection
     public function connection() {
         $server = "localhost";
@@ -77,6 +92,18 @@ class Folder {
 
         $connection = mysqli_connect($server, $user, $password, $db);
         return $connection;
+    }
+
+    public function write_log($log_msg)
+    {
+        $log_filename = "logs";
+        if (!file_exists($log_filename))
+        {
+            mkdir($log_filename, 0777, true);
+        }
+        $log_file_data = $log_filename.'/debug.log';
+        file_put_contents($log_file_data, $log_msg . "\n", FILE_APPEND);
+    
     }
 }
 ?>
